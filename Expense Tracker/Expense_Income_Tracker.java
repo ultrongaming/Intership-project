cb6a0faef6e94a06b262179b67aa234d7d0f4ace
rudra narayan bishoyi <89507313+ultrongaming@users.noreply.github.com>
2023-12-14 20:50:17 +0530

    Add files via upload

---
+++ Expense Tracker/Expense_Income_Tracker.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package expense_income_tracker;
+
+import expense_income_entry.ExpensesIncomesTracker;
+
+/**
+ *
+ * @author  RUDRA NARAYAN BISHOYI
+ */
+public class Expense_Income_Tracker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        new ExpensesIncomesTracker().setLocationRelativeTo(null);
+        
+    }
+
+}
